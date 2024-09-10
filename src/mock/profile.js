b5f766bcd19334d599547676ddf9be79f77600ab
AlexYin <yjyalex1594@gmail.com>
2024-09-10 18:30:26 +0800

    初始化项目

---
+++ src/mock/profile.js
@@ -0,0 +1,13 @@
+export default {
+    profile:{
+        "code" : "10000",
+        "success" : true,
+        "message" : "登录成功",
+        "data" : {
+            mobile:"138000138000",
+            username:"张三ok",
+            company:"12345678",
+            email:"0000@qq.cn",
+        }
+    }
+}
